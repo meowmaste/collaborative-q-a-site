5e5799f04ed992fedae63aa474087f128f4b64e1
Joslyn <jrosbrook@gmail.com>
2014-01-28 15:50:57 -0800

    add devise and user_sign_in partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 CollaborativeQASite::Application.routes.draw do
+  devise_for :users
   resources :question_answers
+
+  root :to => "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
